36f735f3483c658e43b95db0d997017674988ee4
maurosj77 <mauroluna77@hotmail.com>
2022-06-15 00:21:16 -0300

    .

--- Javascript_avanzado/clase3/eje_11.js
+++ Javascript_avanzado/clase3/eje_11.js
@@ -1,5 +1,10 @@
-objet = [{nombre: "Joaco", dni: 26}, {nombre: "Seba",dni: 25},{nombre: "Juan",dni: 23}, {nombre: "Cristian",dni: 20}];
+let array = [
+  { nombre: "Joaco", dni: 26 },
+  { nombre: "Seba", dni: 25 },
+  { nombre: "Juan", dni: 23 },
+  { nombre: "Cristian", dni: 20 },
+];
 
-let pass = objet.map( x => x.dni);
+let pass = array.map((x) => x.dni);
 
 console.log(pass);
