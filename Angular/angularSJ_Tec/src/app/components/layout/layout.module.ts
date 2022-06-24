22ee3d0fd46f57980511a9c5e473b58c94258a47
Mauro Luna <mauroluna77@hotmail.com>
2022-06-24 00:39:01 -0300

    .

--- Angular/angularSJ_Tec/src/app/components/layout/layout.module.ts
+++ Angular/angularSJ_Tec/src/app/components/layout/layout.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
-    HeaderComponent
-  ],
+    HeaderComponent,
+    FooterComponent
+  ],exports:[HeaderComponent],
   imports: [
     CommonModule
   ]
