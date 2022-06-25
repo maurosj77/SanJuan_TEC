92f67112846d8fb2f7cccefc5d85afb4ea6ed730
Mauro Luna <mauroluna77@hotmail.com>
2022-06-24 21:34:45 -0300

    .

---
+++ Javascript_avanzado/clase5/eje_1.js
@@ -0,0 +1,37 @@
+/*function creaFunc(nombre) {
+    var saludo = "Hola";
+    function muestraNombre() {
+        console.log(saludo + " "+ nombre);
+    }
+    return muestraNombre();
+  }
+  function ejemplo(){
+  var miFunc = creaFunc("joaquin");
+  return miFunc
+  }
+  ejemplo();
+*/
+  function creaPrecio(precio){
+    var calculo = "La ganacia es";
+
+        function calculo(entrega){
+            return console.log(calculo+(precio-entrega));
+        }
+  }
+
+  creaPrecio(25)
+
+
+
+  /*function ganancia(precio) {
+  var precio = 1000;
+  return function ganancia() {
+    console.log(`La ganancia de la casa de comidas es de ${precio - entrega}`);
+  };
+}
+
+function entrega() {
+  var precioEntrega = ganancia(150);
+  return precioEntrega;
+}
+entrega();*/
---
+++ Javascript_avanzado/clase5/eje_2.js
@@ -0,0 +1,11 @@
+
+
+function counter(){
+        let inicio = 0;
+
+        function sumar(inicio){
+            inicio ++;
+        }
+return sumar();
+}
+
