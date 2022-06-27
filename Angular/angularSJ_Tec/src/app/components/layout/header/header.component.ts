15a44da6d2625c8ab1865fe70db23a640724f583
Mauro Luna <mauroluna77@hotmail.com>
2022-06-27 20:36:04 -0300

    .

--- Angular/angularSJ_Tec/src/app/components/layout/header/header.component.ts
+++ Angular/angularSJ_Tec/src/app/components/layout/header/header.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  title = 'en la casa de una abuela';
+  cardInfo: any = {
+    title: 'kakita',
+    imaGen:
+      'https://gdm-catalog-fmapi-prod.imgix.net/ProductLogo/f09f8452-ca18-44b5-a3eb-495c93401121.png?auto=format&q=50&w=90&h=90&fit=max&dpr=3',
+    carBody: '',
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
