422727123af6765ac053e215d6bc18419eeff61b
Mauro Luna <mauroluna77@hotmail.com>
2022-06-29 22:03:04 -0300

    .

---
+++ Javascript_avanzado/clase6/item4.js
@@ -0,0 +1,27 @@
+
+
+var instructor = {
+
+nombre: "Joaquin",
+
+edad: 25
+
+}
+
+var alumno = {
+
+nombre: "Marcos",
+
+curso: "FullStack"
+
+}
+
+function getNombre(){
+
+return this.nombre;
+
+}
+
+// Escribir código sin modificar lo que ya se encuentra escrito arriba para
+//poder llamar al método getNombre primero para obtener el nombre del
+//instructor y luego para obtener el nombre del alumno.
